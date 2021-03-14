37baa65288de6f71c966e2a963f5bcca15525191
romarios1987 <romios047@gmail.com>
2021-03-14 22:28:39 +0200

    added views for pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,62 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
+    path: "/login",
+    name: "Login",
+    meta: { layout: "empty" },
+    component: () => import("../views/Login")
+  },
+  {
+    path: "/register",
+    name: "Register",
+    meta: { layout: "empty" },
+    component: () => import("../views/Register")
+  },
+  {
     path: "/",
     name: "Home",
-    component: Home
+    meta: { layout: "main" },
+    component: () => import("../views/Home")
+  },
+  {
+    path: "/categories",
+    name: "Categories",
+    meta: { layout: "main" },
+    component: () => import("../views/Categories")
+  },
+  {
+    path: "/history",
+    name: "History",
+    meta: { layout: "main" },
+    component: () => import("../views/History")
+  },
+  {
+    path: "/planning",
+    name: "Planning",
+    meta: { layout: "main" },
+    component: () => import("../views/Planning")
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    meta: { layout: "main" },
+    component: () => import("../views/Profile")
+  },
+  {
+    path: "/record",
+    name: "Record",
+    meta: { layout: "main" },
+    component: () => import("../views/Record")
+  },
+  {
+    path: "/detail-record",
+    name: "DetailRecord",
+    meta: { layout: "main" },
+    component: () => import("../views/DetailRecord")
   }
 ];
 
